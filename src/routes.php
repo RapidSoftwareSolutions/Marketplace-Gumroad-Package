6d5d16eb5bf3c997da710ca24311b79c62c7462e
ivz-dev <zakutynsky@yahoo.com>
2017-06-22 11:44:58 +0300

    read blocks

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,39 @@
 <?php
 $routes = [
     'metadata',
-    'getUserProducts'
+    'getUserProducts',
+    'getSingleProduct',
+    'deleteSingleProduct',
+    'enableSingleProduct',
+    'disableSingleProduct',
+    'createProductVariantCategory',
+    'getProductSingleVariantCategory',
+    'updateProductVariantCategory',
+    'deleteProductVariantCategory',
+    'getProductVariantCategories',
+    'createProductVariant',
+    'getSingleProductVariant',
+    'updateProductVariant',
+    'deleteProductVariant',
+    'getVariantsInVariantCategory',
+    'getProductOfferCodes',
+    'getSingleProductOfferCode',
+    'createProductOfferCode',
+    'updateProductOfferCode',
+    'deleteProductOfferCode',
+    'getProductCustomFields',
+    'createProductCustomField',
+    'updateProductCustomField',
+    'deleteProductCustomField',
+    'getUser',
+    'getUserSubscriptions',
+    'subscribeToResource',
+    'unsubscribeFromResource',
+    'getUserSucessfulSales',
+    'getUserSingleSale',
+    'getProductActiveSubscribers',
+    'getProductSingleSubscriber'
+
 ];
 foreach($routes as $file) {
     require __DIR__ . '/../src/routes/'.$file.'.php';
